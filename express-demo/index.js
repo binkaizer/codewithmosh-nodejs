a65172d393eca06a2f48b7260b0ab67fa6794e82
Tam Pham <binemperor@gmail.com>
2018-12-24 22:38:27 +0700

    7-Configuration, 8-Debugging
    7-Configuration, 8-Debugging

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,11 +1,35 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
 const logger = require('./logger')
 const express = require('express');
 const app = express();
 
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`); //undefined
+console.log(`app: ${app.get('env')}`);
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); //key=value&key=value
 app.use(express.static('public'));
+app.use(helmet());
+
+//Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    // console.log('Morgan enable...');
+    startupDebugger('Morgan enabled...');
+}
+
+//Db work...
+dbDebugger('Connected to the database...');
+
 
 app.use(logger);
 
