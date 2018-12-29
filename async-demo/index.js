2be3620ea1e9dfa43a57ea7602cea1da7c473d21
Tam Pham <binemperor@gmail.com>
2018-12-29 21:55:48 +0700

    Update index.js

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,6 +1,10 @@
 console.log('Before');
-getUser(1, function(user){
+getUser(1, (user) => {
     console.log('User', user);
+
+    getRepositories(user.gitHubUsername, (repos) => {
+        console.log('Repos', repos);
+    });
 });
 console.log('After');
 
@@ -9,5 +13,12 @@ function getUser(id, callback) {
         console.log('Reading a user from a database...');
         callback({id: id, gitHubUsername: 'mosh'});
     }, 2000);
+}
+
+function getRepositories(username, callback){
+    setTimeout(() => {
+        console.log('Calling GitHub API...');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000)
 }
 
